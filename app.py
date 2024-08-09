b9b6b4b5730e32503ae020dcf50e9f0af4c04441
Yi Chen <chenyiapp@yahoo.com>
2024-08-08 17:01:12 -0700

    set up bedrock api

--- app.py
+++ app.py
@@ -8,11 +8,17 @@ bedrock_runtime=boto3.client('bedrock-runtime', region_name='us-west-2')
 
 prompt="What is the date today?"
 
+# kwargs={
+#  "modelId": "meta.llama3-1-405b-instruct-v1:0",
+#  "contentType": "application/json",
+#  "accept": "application/json",
+#  "body": json.dumps({"prompt":"this is where you place your input text","max_gen_len":512,"temperature":0.5,"top_p":0.9})
+# }
 kwargs={
- "modelId": os.getenv("BEDROCK_MODEL_ID"),
+ "modelId": "meta.llama3-8b-instruct-v1:0",
  "contentType": "application/json",
  "accept": "application/json",
- "body": json.dumps({"prompt":"this is where you place your input text","max_gen_len":512,"temperature":0.5,"top_p":0.9})
+ "body": json.dumps({"prompt":"What country is San Francisco located?","max_gen_len":512,"temperature":0.5,"top_p":0.9})
 }
 
 res=bedrock_runtime.invoke_model(**kwargs)
